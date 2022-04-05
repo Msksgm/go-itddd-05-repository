0831b1365bae3d6f999d9d8a6515eba45ce456c8
Msksgm <msksgm3124@gmail.com>
2022-04-06 06:52:06 +0900

    add: userを追加

---
+++ domain/model/user/user.go
@@ -0,0 +1,14 @@
+package user
+
+type User struct {
+	id   UserId
+	name UserName
+}
+
+func NewUser(userId UserId, userName UserName) (*User, error) {
+	return &User{id: userId, name: userName}, nil
+}
+
+func (user *User) Id() *UserId {
+	return &user.id
+}
